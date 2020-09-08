8e94d706371f7b729a7fbdc376074cdf176267b8
Tomislav Hop <thop@upchain.com>
2020-09-08 12:32:07 +0200

    Initial

---
+++ src/main/java/com/example/testStyleGuide/TestStyleGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.testStyleGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStyleGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStyleGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testStyleGuide/TestStyleGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testStyleGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStyleGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
